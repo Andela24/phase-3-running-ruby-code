a68821c970810d540fa16affb9c8a6a9b8e963f0
Andela Stevanovic <andjela.s166@gmail.com>
2022-03-02 12:38:45 -0500

    commit tree 8577cf0cd1bb8ee94104fc921350c56cb812762b

---
+++ app.rb
@@ -0,0 +1,8 @@
+#comment
+# puts "Hello World!"
+# print "Hello world from print"
+# puts [1, 2, 3].inspect
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+puts "Hello World!"
+print "Pass this test, please."
+p [1, 2, 3]
